79ee213fc9152fd4edd189df0da5bcf9bc903a40
skyler.su <thatsummers@gmail.com>
2026-05-16 22:18:44 +0800

    feat: adapte new web ui

--- patch-hermes-web-ui.js
+++ patch-hermes-web-ui.js
@@ -3,20 +3,32 @@ const path = "/usr/local/lib/node_modules/hermes-web-ui/dist/server/index.js";
 
 let source = fs.readFileSync(path, "utf8");
 
-const original =
-  'O=U?.models?.length?[...U.models]:[i.model];if(!U&&i.api_key)try{let S=await _2(k,i.api_key);S.length>0&&(O=[...new Set([i.model,...S])])}catch{}';
+const patterns = [
+  {
+    original:
+      'O=U?.models?.length?[...U.models]:[i.model];if(!U&&i.api_key)try{let S=await _2(k,i.api_key);S.length>0&&(O=[...new Set([i.model,...S])])}catch{}',
+    replacement:
+      'O=U?.models?.length?[...U.models]:Array.from(new Set([i.model,...Object.keys(i.models||{})]));if(!U&&i.api_key)try{let S=await _2(k,i.api_key);S.length>0&&(O=[...new Set([i.model,...Object.keys(i.models||{}),...S])])}catch{}',
+  },
+  {
+    original:
+      'O=U?.models?.length?[...U.models]:[i.model];if(!U&&i.api_key)try{let f=await q2(k,i.api_key);f.length>0&&(O=[...new Set([i.model,...f])])}catch{}',
+    replacement:
+      'O=U?.models?.length?[...U.models]:Array.from(new Set([i.model,...Object.keys(i.models||{})]));if(!U&&i.api_key)try{let f=await q2(k,i.api_key);f.length>0&&(O=[...new Set([i.model,...Object.keys(i.models||{}),...f])])}catch{}',
+  },
+];
 
-const replacement =
-  'O=U?.models?.length?[...U.models]:Array.from(new Set([i.model,...Object.keys(i.models||{})]));if(!U&&i.api_key)try{let S=await _2(k,i.api_key);S.length>0&&(O=[...new Set([i.model,...Object.keys(i.models||{}),...S])])}catch{}';
-
-if (!source.includes(original)) {
+for (const { original, replacement } of patterns) {
+  if (source.includes(original)) {
+    source = source.replace(original, replacement);
+    fs.writeFileSync(path, source);
+    console.log("patched hermes-web-ui custom provider model expansion");
+    process.exit(0);
+  }
   if (source.includes(replacement)) {
     console.log("hermes-web-ui custom provider model expansion already patched");
     process.exit(0);
   }
-  throw new Error("hermes-web-ui custom provider model block was not found");
 }
 
-source = source.replace(original, replacement);
-fs.writeFileSync(path, source);
-console.log("patched hermes-web-ui custom provider model expansion");
+throw new Error("hermes-web-ui custom provider model block was not found");
